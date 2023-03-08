4622805ba460b81943ca1ca662128f8d75122df1
tktk4751 <tkworld.4751@gmail.com>
2023-03-08 09:22:49 +0900

    new

---
+++ src/items/items.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+
+    return this.itemsService.create(item);
+  }
+}
